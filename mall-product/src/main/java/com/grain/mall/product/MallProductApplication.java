2b9bee4642c309023ef506ab2965b9567dc35349
DragonWen <18475536452@163.com>
2020-05-22 17:29:39 +0800

    添加代码注释

--- mall-product/src/main/java/com/grain/mall/product/MallProductApplication.java
+++ mall-product/src/main/java/com/grain/mall/product/MallProductApplication.java
@@ -3,6 +3,14 @@ package com.grain.mall.product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author：Dragon Wen
+ * @email：18475536452@163.com
+ * @date：Created in 2020/5/21 16:45
+ * @description：
+ * @modified By：
+ * @version: $
+ */
 @SpringBootApplication
 public class MallProductApplication {
 
