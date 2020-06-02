fdc0c4006664208884fd1ce82c59696252732d1f
DragonWen <18475536452@163.com>
2020-06-02 16:48:44 +0800

    测试微服务基本CRUD功能

--- mall-product/src/main/java/com/grain/mall/product/service/impl/AttrAttrgroupRelationServiceImpl.java
+++ mall-product/src/main/java/com/grain/mall/product/service/impl/AttrAttrgroupRelationServiceImpl.java
@@ -1,12 +1,13 @@
 package com.grain.mall.product.service.impl;
 
-import com.baomidou.mybatisplus.core.conditions.query.Query;
 import org.springframework.stereotype.Service;
 import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 
+import com.grain.common.utils.PageUtils;
+import com.grain.common.utils.Query;
 import com.grain.mall.product.dao.AttrAttrgroupRelationDao;
 import com.grain.mall.product.entity.AttrAttrgroupRelationEntity;
 import com.grain.mall.product.service.AttrAttrgroupRelationService;
