3784a9f4c51dfff671631131731ebaf28d1761aa
DragonWen <18475536452@163.com>
2020-06-16 11:09:48 +0800

    商品服务-API-三级分类-网关统一配置跨域

---
+++ mall-gateway/src/main/java/com/grain/mall/gateway/config/MallCorsConfiguration.java
@@ -0,0 +1,35 @@
+package com.grain.mall.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+/**
+ * @author：Dragon Wen
+ * @email：18475536452@163.com
+ * @date：Created in 2020/6/16 10:50
+ * @description：处理跨域配置文件
+ * @modified By：
+ * @version: $
+ */
+@Configuration
+public class MallCorsConfiguration {
+
+    @Bean
+    public CorsWebFilter corsWebFilter(){
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+        //1、配置跨域
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.setAllowCredentials(true);
+
+        source.registerCorsConfiguration("/**",corsConfiguration);
+        return new CorsWebFilter(source);
+    }
+}
