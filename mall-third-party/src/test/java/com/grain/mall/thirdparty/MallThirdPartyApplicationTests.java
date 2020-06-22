01e093561b1563fdd6768d88ef018ae587051f52
DragonWen <18475536452@163.com>
2020-06-22 17:19:26 +0800

    商品服务-API-品牌管理-OSS获取服务端签名

---
+++ mall-third-party/src/test/java/com/grain/mall/thirdparty/MallThirdPartyApplicationTests.java
@@ -0,0 +1,39 @@
+package com.grain.mall.thirdparty;
+
+import com.aliyun.oss.OSSClient;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MallThirdPartyApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+    @Autowired
+    OSSClient ossClient;
+
+    @Test
+    public void testUpload() throws FileNotFoundException {
+
+        // 上传文件流。
+        InputStream inputStream = new FileInputStream("C:\\Users\\DragonWen\\Desktop\\小程序图标\\weichart.png");
+
+        ossClient.putObject("grain-mall-dragon", "weichart.jpg", inputStream);
+
+        // 关闭OSSClient。
+        ossClient.shutdown();
+
+        System.out.println("上传完成...");
+    }
+
+}
