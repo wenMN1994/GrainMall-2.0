d2493f2a0eaeaa09b8f66af9c9cb0d24e4d36cbd
DragonWen <18475536452@163.com>
2020-06-23 13:42:44 +0800

    商品服务-API-品牌管理-前端表单校验&自定义校验器

--- mall-ui/renren-fast-vue/src/views/modules/product/brand-add-or-update.vue
+++ mall-ui/renren-fast-vue/src/views/modules/product/brand-add-or-update.vue
@@ -44,9 +44,9 @@ import SingleUpload from "@/components/upload/singleUpload";
           name: '',
           logo: '',
           descript: '',
-          showStatus: '',
+          showStatus: 1,
           firstLetter: '',
-          sort: ''
+          sort: 0
         },
         dataRule: {
           name: [
@@ -62,10 +62,26 @@ import SingleUpload from "@/components/upload/singleUpload";
             { required: true, message: '显示状态[0-不显示；1-显示]不能为空', trigger: 'blur' }
           ],
           firstLetter: [
-            { required: true, message: '检索首字母不能为空', trigger: 'blur' }
+            { validator: (rule, value, callback)=>{
+              if(value == ''){
+                callback(new Error('首字母必须填写'))
+              } else if(!/^[a-zA-Z]$/.test(value)){
+                callback(new Error("首字母必须a-z或者A-Z之间"));
+              } else {
+                callback();
+              }
+            }, trigger: 'blur' }
           ],
           sort: [
-            { required: true, message: '排序不能为空', trigger: 'blur' }
+            { validator: (rule, value, callback)=>{
+              if (value == "") {
+                callback(new Error("排序字段必须填写"));
+              } else if (!Number.isInteger(value) || value<0) {
+                callback(new Error("排序必须是一个大于等于0的整数"));
+              } else {
+                callback();
+              }
+            }, trigger: 'blur' }
           ]
         }
       }
