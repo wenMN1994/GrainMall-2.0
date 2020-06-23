02c8e7b254db7e55f650baf4a0a73d405cee70b1
DragonWen <18475536452@163.com>
2020-06-23 13:32:37 +0800

    商品服务-API-品牌管理-OSS前后联调测试上传

--- mall-third-party/src/main/java/com/grain/mall/thirdparty/controller/OssController.java
+++ mall-third-party/src/main/java/com/grain/mall/thirdparty/controller/OssController.java
@@ -4,6 +4,7 @@ import com.aliyun.oss.OSS;
 import com.aliyun.oss.common.utils.BinaryUtil;
 import com.aliyun.oss.model.MatchMode;
 import com.aliyun.oss.model.PolicyConditions;
+import com.grain.common.utils.R;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -38,7 +39,7 @@ public class OssController {
     private String bucket; // 请填写您的 bucketname 。
 
     @RequestMapping("/oss/policy")
-    public Map<String, String> policy(){
+    public R policy(){
 
         String host = "https://" + bucket + "." + endpoint; // host的格式为 bucketname.endpoint
         // callbackUrl为 上传回调服务器的URL，请将下面的IP和Port配置为您自己的真实信息。
@@ -75,6 +76,6 @@ public class OssController {
         } finally {
             ossClient.shutdown();
         }
-        return respMap;
+        return R.ok().put("data", respMap);
     }
 }
