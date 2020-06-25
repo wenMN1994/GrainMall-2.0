0421bb3995572a4ae66fbadd18fc2bf0da34f60f
DragonWen <18475536452@163.com>
2020-06-25 16:59:32 +0800

    商品服务-API-平台属性-规格参数新增与VO

--- mall-product/src/main/java/com/grain/mall/product/service/impl/AttrServiceImpl.java
+++ mall-product/src/main/java/com/grain/mall/product/service/impl/AttrServiceImpl.java
@@ -1,5 +1,10 @@
 package com.grain.mall.product.service.impl;
 
+import com.grain.mall.product.dao.AttrAttrgroupRelationDao;
+import com.grain.mall.product.entity.AttrAttrgroupRelationEntity;
+import com.grain.mall.product.vo.AttrVo;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
@@ -11,10 +16,14 @@ import com.grain.common.utils.Query;
 import com.grain.mall.product.dao.AttrDao;
 import com.grain.mall.product.entity.AttrEntity;
 import com.grain.mall.product.service.AttrService;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @Service("attrService")
 public class AttrServiceImpl extends ServiceImpl<AttrDao, AttrEntity> implements AttrService {
+
+    @Autowired
+    AttrAttrgroupRelationDao relationDao;
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
@@ -24,6 +33,21 @@ public class AttrServiceImpl extends ServiceImpl<AttrDao, AttrEntity> implements
         );
 
         return new PageUtils(page);
+    }
+
+    @Transactional
+    @Override
+    public void saveAttr(AttrVo attr) {
+        AttrEntity attrEntity = new AttrEntity();
+        BeanUtils.copyProperties(attr, attrEntity);
+        // 保存基本数据
+        this.save(attrEntity);
+
+        // 保存关联关系
+        AttrAttrgroupRelationEntity relationEntity = new AttrAttrgroupRelationEntity();
+        relationEntity.setAttrGroupId(attr.getAttrGroupId());
+        relationEntity.setAttrId(attrEntity.getAttrId());
+        relationDao.insert(relationEntity);
     }
 
 }
