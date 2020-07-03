e36fb3d2f153c905ff03cd772236d6e525ea711d
DragonWen <18475536452@163.com>
2020-07-03 16:50:53 +0800

    全文检索-ElasticSearch-整合-测试复杂检索

--- mall-search/src/test/java/com/grain/mall/search/MallSearchApplicationTests.java
+++ mall-search/src/test/java/com/grain/mall/search/MallSearchApplicationTests.java
@@ -3,10 +3,23 @@ package com.grain.mall.search;
 import com.alibaba.fastjson.JSON;
 import com.grain.mall.search.config.MallElasticSearchConfig;
 import lombok.Data;
+import lombok.ToString;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.Aggregations;
+import org.elasticsearch.search.aggregations.bucket.terms.Terms;
+import org.elasticsearch.search.aggregations.bucket.terms.TermsAggregationBuilder;
+import org.elasticsearch.search.aggregations.metrics.Avg;
+import org.elasticsearch.search.aggregations.metrics.AvgAggregationBuilder;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +34,22 @@ public class MallSearchApplicationTests {
 
     @Autowired
     private RestHighLevelClient client;
+
+    @ToString
+    @Data
+    static class Account {
+        private int account_number;
+        private int balance;
+        private String firstname;
+        private String lastname;
+        private int age;
+        private String gender;
+        private String address;
+        private String employer;
+        private String email;
+        private String city;
+        private String state;
+    }
 
     @Test
     public void contextLoads() {
@@ -50,6 +79,57 @@ public class MallSearchApplicationTests {
         private String userName;
         private String gender;
         private Integer age;
+    }
+
+    @Test
+    public void searchData() throws IOException {
+        // 1、创建检索请求
+        SearchRequest searchRequest = new SearchRequest();
+        // 指定索引
+        searchRequest.indices("bank");
+        // 指定DSL，检索条件
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        // 1.1、构造检索条件
+        searchSourceBuilder.query(QueryBuilders.matchQuery("address", "mill"));
+
+        // 1.2、按照年龄的值分布进行聚合
+        TermsAggregationBuilder ageAgg = AggregationBuilders.terms("ageAgg").field("age").size(10);
+        searchSourceBuilder.aggregation(ageAgg);
+
+        // 1.3、计算平均薪资
+        AvgAggregationBuilder balanceAvg = AggregationBuilders.avg("balanceAvg").field("balance");
+        searchSourceBuilder.aggregation(balanceAvg);
+
+        System.out.println("检索条件：" + searchSourceBuilder.toString());
+        searchRequest.source(searchSourceBuilder);
+
+        // 2、执行检索
+        SearchResponse searchResponse = client.search(searchRequest, MallElasticSearchConfig.COMMON_OPTIONS);
+
+        // 3、分析结果
+        System.out.println(searchResponse.toString());
+        // 3.1、获取所有查到的数据
+        SearchHits hits = searchResponse.getHits();
+        SearchHit[] searchHits = hits.getHits();
+        for (SearchHit hit : searchHits ) {
+            String string = hit.getSourceAsString();
+            Account account = JSON.parseObject(string, Account.class);
+            System.out.println("account" + account.toString());
+        }
+
+        // 3.2、获取这次检索到的分析信息
+        Aggregations aggregations = searchResponse.getAggregations();
+        /*for (Aggregation aggregation : aggregations.asList()) {
+            System.out.println("当前聚合：" + aggregation.getName());
+        }*/
+        Terms aggregation_ageAgg = aggregations.get("ageAgg");
+        for (Terms.Bucket bucket : aggregation_ageAgg.getBuckets()) {
+            String keyAsString = bucket.getKeyAsString();
+            System.out.println("年龄：" + keyAsString + "===>" + bucket.getDocCount());
+        }
+
+        Avg aggregation_balanceAvg = aggregations.get("balanceAvg");
+        System.out.println("平均薪资：" + aggregation_balanceAvg.getValue());
     }
 
 }
