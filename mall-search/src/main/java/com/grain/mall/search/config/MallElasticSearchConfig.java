730d2b02eaacf4ed80d04950b7d7a01f347caaaf
DragonWen <18475536452@163.com>
2020-07-03 14:58:46 +0800

    全文检索-ElasticSearch-整合-测试保存

--- mall-search/src/main/java/com/grain/mall/search/config/MallElasticSearchConfig.java
+++ mall-search/src/main/java/com/grain/mall/search/config/MallElasticSearchConfig.java
@@ -1,6 +1,8 @@
 package com.grain.mall.search.config;
 
 import org.apache.http.HttpHost;
+import org.elasticsearch.client.HttpAsyncResponseConsumerFactory;
+import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.context.annotation.Bean;
@@ -16,6 +18,16 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class MallElasticSearchConfig {
+
+    public static final RequestOptions COMMON_OPTIONS;
+    static {
+        RequestOptions.Builder builder = RequestOptions.DEFAULT.toBuilder();
+//        builder.addHeader("Authorization", "Bearer " + TOKEN);
+//        builder.setHttpAsyncResponseConsumerFactory(
+//                new HttpAsyncResponseConsumerFactory
+//                        .HeapBufferedResponseConsumerFactory(30 * 1024 * 1024 * 1024));
+        COMMON_OPTIONS = builder.build();
+    }
 
     @Bean
     public RestHighLevelClient esRestClient(){
--- mall-search/src/test/java/com/grain/mall/search/MallSearchApplicationTests.java
+++ mall-search/src/test/java/com/grain/mall/search/MallSearchApplicationTests.java
@@ -1,11 +1,19 @@
 package com.grain.mall.search;
 
+import com.alibaba.fastjson.JSON;
+import com.grain.mall.search.config.MallElasticSearchConfig;
+import lombok.Data;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -17,6 +25,31 @@ public class MallSearchApplicationTests {
     @Test
     public void contextLoads() {
         System.out.println(client);
+    }
+
+    @Test
+    public void indexData() throws IOException {
+        IndexRequest indexRequest = new IndexRequest("users");
+        indexRequest.id("1"); // 数据的id
+        User user = new User();
+        user.setUserName("DragonWen");
+        user.setAge(27);
+        user.setGender("男");
+        String jsonString = JSON.toJSONString(user);
+        indexRequest.source(jsonString, XContentType.JSON); // 要保存的内容
+
+        // 执行操作
+        IndexResponse index = client.index(indexRequest, MallElasticSearchConfig.COMMON_OPTIONS);
+
+        // 提取有用的响应数据
+        System.out.println(index);
+    }
+
+    @Data
+    class User{
+        private String userName;
+        private String gender;
+        private Integer age;
     }
 
 }
