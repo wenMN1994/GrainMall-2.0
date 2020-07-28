0f16a76abf450eeb20e04ff60be5f787edcecde2
DragonWen <18475536452@163.com>
2020-07-28 22:46:59 +0800

    商城业务-认证服务-整合短信验证码

---
+++ mall-third-party/src/main/java/com/grain/mall/thirdparty/component/SmsComponent.java
@@ -0,0 +1,51 @@
+package com.grain.mall.thirdparty.component;
+
+import com.grain.mall.thirdparty.utils.HttpUtils;
+import lombok.Data;
+import org.apache.http.HttpResponse;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author：Dragon Wen
+ * @email：18475536452@163.com
+ * @date：Created in 2020/7/28 22:21
+ * @description：
+ * @modified By：
+ * @version: $
+ */
+@ConfigurationProperties(prefix = "spring.cloud.alicloud.sms")
+@Data
+@Component
+public class SmsComponent {
+
+    private String host;
+    private String path;
+    private String method;
+    private String appcode;
+    private String tplId;
+
+    public void sendSmsCode(String mobile, String code){
+        Map<String, String> headers = new HashMap<String, String>();
+        //最后在header中的格式(中间是英文空格)为Authorization:APPCODE 83359fd73fe94948385f570e3c139105
+        headers.put("Authorization", "APPCODE " + appcode);
+        Map<String, String> querys = new HashMap<String, String>();
+        querys.put("mobile", mobile);
+        querys.put("param", "code:"+code);
+        querys.put("tpl_id", tplId);
+        Map<String, String> bodys = new HashMap<String, String>();
+
+
+        try {
+            HttpResponse response = HttpUtils.doPost(host, path, method, headers, querys, bodys);
+            System.out.println(response.toString());
+            //获取response的body
+            //System.out.println(EntityUtils.toString(response.getEntity()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
--- mall-third-party/src/test/java/com/grain/mall/thirdparty/MallThirdPartyApplicationTests.java
+++ mall-third-party/src/test/java/com/grain/mall/thirdparty/MallThirdPartyApplicationTests.java
@@ -1,6 +1,9 @@
 package com.grain.mall.thirdparty;
 
 import com.aliyun.oss.OSSClient;
+import com.grain.mall.thirdparty.component.SmsComponent;
+import com.grain.mall.thirdparty.utils.HttpUtils;
+import org.apache.http.HttpResponse;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +13,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -21,6 +26,40 @@ public class MallThirdPartyApplicationTests {
 
     @Autowired
     OSSClient ossClient;
+
+    @Autowired
+    SmsComponent smsComponent;
+
+    @Test
+    public void testSendCode(){
+        smsComponent.sendSmsCode("18475536452","413709");
+    }
+
+    @Test
+    public void sendSms(){
+        String host = "http://dingxintz.market.alicloudapi.com";
+        String path = "/dx/notifySms";
+        String method = "POST";
+        String appcode = "你自己的AppCode";
+        Map<String, String> headers = new HashMap<String, String>();
+        //最后在header中的格式(中间是英文空格)为Authorization:APPCODE 83359fd73fe94948385f570e3c139105
+        headers.put("Authorization", "APPCODE " + appcode);
+        Map<String, String> querys = new HashMap<String, String>();
+        querys.put("mobile", "18475536452");
+        querys.put("param", "code:413709");
+        querys.put("tpl_id", "TP19121716");
+        Map<String, String> bodys = new HashMap<String, String>();
+
+
+        try {
+            HttpResponse response = HttpUtils.doPost(host, path, method, headers, querys, bodys);
+            System.out.println(response.toString());
+            //获取response的body
+            //System.out.println(EntityUtils.toString(response.getEntity()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     @Test
     public void testUpload() throws FileNotFoundException {
