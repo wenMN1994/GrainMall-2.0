b3bf8eef19e2d61902fd0823cae15f52063840e1
DragonWen <18475536452@163.com>
2020-08-06 22:24:43 +0800

    商城业务-订单服务-页面环境搭建

---
+++ mall-order/src/main/java/com/grain/mall/order/web/HelloController.java
@@ -0,0 +1,25 @@
+package com.grain.mall.order.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import javax.websocket.server.PathParam;
+
+/**
+ * @author：Dragon Wen
+ * @email：18475536452@163.com
+ * @date：Created in 2020/8/6 22:08
+ * @description：
+ * @modified By：
+ * @version: $
+ */
+@Controller
+public class HelloController {
+
+    @GetMapping("/{page}.html")
+    public String listPage(@PathVariable("page") String page){
+
+        return page;
+    }
+}
