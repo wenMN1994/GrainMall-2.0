201b819801728d1c30296dfdf031a35bc4e26c3c
DragonWen <18475536452@163.com>
2020-09-04 16:09:37 +0800

    商城业务-创建秒杀服务

---
+++ mall-seckill/src/main/java/com/grain/mall/seckill/MallSeckillApplication.java
@@ -0,0 +1,18 @@
+package com.grain.mall.seckill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author DragonWen
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class MallSeckillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallSeckillApplication.class, args);
+    }
+
+}
---
+++ mall-seckill/src/test/java/com/grain/mall/seckill/MallSeckillApplicationTests.java
@@ -0,0 +1,16 @@
+package com.grain.mall.seckill;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MallSeckillApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
